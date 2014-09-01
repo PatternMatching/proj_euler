ee2ad5b02bf26d2dedf1c00656506dd201fe1eb7
Peter Hennings <phennings@Macintosh.local>
2014-09-01 19:09:37 -0400

    Initial commit of attempt at solution to Euler #17

---
+++ prob_17.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+"""
+Problem 17
+
+If the numbers 1 to 5 are written out in words: one, two, three, four, 
+five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written 
+out in words, how many letters would be used?
+
+"""
+
+NUM_WORD_MAP = {1 : 'one',
+                2 : 'two',
+                3 : 'three',
+                4 : 'four',
+                5 : 'five',
+                6 : 'six',
+                7 : 'seven',
+                8 : 'eight',
+                9 : 'nine',
+                10 : 'ten',
+                11 : 'eleven',
+                12 : 'twelve',
+                13 : 'thirteen',
+                14 : 'fourteen',
+                15 : 'fifteen',
+                16 : 'sixteen',
+                17 : 'seventeen',
+                18 : 'eighteen',
+                19 : 'nineteen',
+                20 : 'twenty',
+                30 : 'thirty',
+                40 : 'forty',
+                50 : 'fifty',
+                60 : 'sixty',
+                70 : 'seventy',
+                80 : 'eighty',
+                90 : 'ninety'}
+
+def print_num(n):
+    """ Want to print English representation of n """
+    n_digits = len(str(n))
+    
+    # We presume that we will not be seeing numbers with > 4 digits
+    if n_digits == 1:
+        return NUM_WORD_MAP[n]
+    elif n_digits == 2:
+        # If n > 20, need to use modulus to get name of tens digit
+        if n > 20:
+            tens_num = n / 10 * 10
+            ones_num = n % 10
+            return NUM_WORD_MAP[tens_num] + "-" + NUM_WORD_MAP[ones_num]
+        else:
+            return NUM_WORD_MAP[n]
+    elif n_digits == 3:
+        to_return = ""
+        to_return.join(NUM_WORD_MAP[n / 100] + " hundred")
+        tens_digit = n % 100 / 10
+        ones_digit = n % 10
+        if tens_digit == 0 & ones_digit == 0:
+            return to_return
+        elif tens_digit == 0:
+            to_return.join(" and ",
+                           NUM_WORD_MAP[ones_digit])
+        elif ones_digit == 0:
+            to_return.join(" and ",
+                           NUM_WORD_MAP[tens_digit * 10])
+        else:
+            to_return.join(" and ",
+                           NUM_WORD_MAP[tens_digit * 10],
+                           " ",
+                           NUM_WORD_MAP[ones_digit])
+        return to_return
+    elif n_digits == 4:
+        to_return = ""
+        thou_digit = n / 1000
+        hund_digit = n % 1000 / 100
+        tens_digit = n % 100 / 10
+        ones_digit = n % 10
+        to_return.join(NUM_WORD_MAP[thou_digit] + " thousand")
+        if hund_digit == 0 & tens_digit == 0 & ones_digit == 0:
+            to_return.join("")
+        elif hund_digit == 0 & tens_digit == 0:
+            to_return.join(" and ",
+                           NUM_WORD_MAP[ones_digit])
+        elif tens_digit == 0:
+            to_return.join(" and ",
+                           NUM_WORD_MAP[hund_digit],
+                           " hundred and ",
+                           NUM_WORD_MAP[ones_digit])
+        else:
+            to_return.join(" and ",
+                           NUM_WORD_MAP[hund_digit],
+                           " hundred and ",
+                           NUM_WORD_MAP[tens_digit*10],
+                           " ",
+                           NUM_WORD_MAP[ones_digit])
+        return to_return
+
+if __name__ == "__main__":
+    print print_num(1000)
+    print print_num(100)
+    print print_num(101)
+    print print_num(110)
+    
