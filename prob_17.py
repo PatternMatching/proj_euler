754b4a9d9de24932e38a01bb9171aa1570948b62
PatternMatching <phennings@gmail.com>
2014-10-31 18:01:50 -0400

    Made further progress but hung up and needs further debugging

--- prob_17.py
+++ prob_17.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 """
+Project Euler
 Problem 17
 
 If the numbers 1 to 5 are written out in words: one, two, three, four, 
@@ -8,7 +9,6 @@ five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
 
 If all the numbers from 1 to 1000 (one thousand) inclusive were written 
 out in words, how many letters would be used?
-
 """
 
 NUM_WORD_MAP = {1 : 'one',
@@ -51,57 +51,70 @@ def print_num(n):
         if n > 20:
             tens_num = n / 10 * 10
             ones_num = n % 10
-            return NUM_WORD_MAP[tens_num] + "-" + NUM_WORD_MAP[ones_num]
+            if ones_num == 0:
+                return NUM_WORD_MAP[tens_num]
+            else:
+                return " ".join([NUM_WORD_MAP[tens_num],
+                                 NUM_WORD_MAP[ones_num]])
         else:
             return NUM_WORD_MAP[n]
     elif n_digits == 3:
-        to_return = ""
-        to_return.join(NUM_WORD_MAP[n / 100] + " hundred")
+        to_return = " ".join([NUM_WORD_MAP[n / 100], "hundred"])
         tens_digit = n % 100 / 10
         ones_digit = n % 10
-        if tens_digit == 0 & ones_digit == 0:
-            return to_return
+        if (tens_digit == 0) & (ones_digit == 0):
+            return to_return    
         elif tens_digit == 0:
-            to_return.join(" and ",
-                           NUM_WORD_MAP[ones_digit])
+            to_return = " ".join([to_return,
+                                  "and",
+                                  NUM_WORD_MAP[ones_digit]])
         elif ones_digit == 0:
-            to_return.join(" and ",
-                           NUM_WORD_MAP[tens_digit * 10])
+            to_return = " ".join([to_return,
+                                  "and",
+                                  NUM_WORD_MAP[tens_digit * 10]])
         else:
-            to_return.join(" and ",
-                           NUM_WORD_MAP[tens_digit * 10],
-                           " ",
-                           NUM_WORD_MAP[ones_digit])
+            to_return = " ".join([to_return,
+                                  "and",
+                                  NUM_WORD_MAP[tens_digit * 10],
+                                  NUM_WORD_MAP[ones_digit]])
         return to_return
     elif n_digits == 4:
-        to_return = ""
         thou_digit = n / 1000
         hund_digit = n % 1000 / 100
         tens_digit = n % 100 / 10
         ones_digit = n % 10
-        to_return.join(NUM_WORD_MAP[thou_digit] + " thousand")
+        to_return = " ".join([NUM_WORD_MAP[thou_digit],
+                              "thousand"])
         if hund_digit == 0 & tens_digit == 0 & ones_digit == 0:
-            to_return.join("")
+            return to_return
         elif hund_digit == 0 & tens_digit == 0:
-            to_return.join(" and ",
-                           NUM_WORD_MAP[ones_digit])
+            to_return = " ".join([to_return,
+                                  "and",
+                                  NUM_WORD_MAP[ones_digit]])
         elif tens_digit == 0:
-            to_return.join(" and ",
-                           NUM_WORD_MAP[hund_digit],
-                           " hundred and ",
-                           NUM_WORD_MAP[ones_digit])
+            to_return = " ".join([to_return, 
+                                  "and",
+                                  NUM_WORD_MAP[hund_digit],
+                                  "hundred and",
+                                  NUM_WORD_MAP[ones_digit]])
         else:
-            to_return.join(" and ",
-                           NUM_WORD_MAP[hund_digit],
-                           " hundred and ",
-                           NUM_WORD_MAP[tens_digit*10],
-                           " ",
-                           NUM_WORD_MAP[ones_digit])
+            to_return = " ".join([to_return,
+                                  "and",
+                                  NUM_WORD_MAP[hund_digit],
+                                  "hundred and",
+                                  NUM_WORD_MAP[tens_digit*10],
+                                  NUM_WORD_MAP[ones_digit]])
         return to_return
 
 if __name__ == "__main__":
-    print print_num(1000)
-    print print_num(100)
-    print print_num(101)
-    print print_num(110)
+    num_letters = 0
+    nums_as_strs = []
+    for i in range(1, 1001):
+        num_as_str = print_num(i)
+        nums_as_strs.append(num_as_str)
+        num_letters += len("".join(str.split(num_as_str, " ")))
+    
+    print num_letters
+    
+
     
